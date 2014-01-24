146c2ec239b4bbcfa6d96575c7f2fc422af88b3d
John Grimes <john@smallspark.com.au>
2014-01-24 13:57:03 +1000

    Only drop table with cascade if it exists

--- lib/rails_sql_views/connection_adapters/abstract/schema_statements.rb
+++ lib/rails_sql_views/connection_adapters/abstract/schema_statements.rb
@@ -59,7 +59,7 @@ module RailsSqlViews
       end
 
       def drop_table_with_cascade(table_name, options = {})
-        execute "DROP TABLE #{quote_table_name(table_name)} CASCADE"
+        execute "DROP TABLE IF EXISTS #{quote_table_name(table_name)} CASCADE"
       end
       
       # Drop a view.
